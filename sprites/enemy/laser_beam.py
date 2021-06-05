a9940304a72187990715bcafaef58bd9965f88a9
Baran Eryürek <baran.4821@hotmail.com>
2021-06-06 00:05:39 +0300

    improved laser beam

--- sprites/enemy/laser_beam.py
+++ sprites/enemy/laser_beam.py
@@ -9,27 +9,35 @@ class LaserBeam(pygame.sprite.Sprite):
     def __init__(self, player):
         self.player = player
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((WIDTH, TILE_H*0.5))
-        self.image.fill(WHITE)
+        self.beam_frame = pygame.image.load("resources/image/enemy/beam.png").convert_alpha()
+        self.image = pygame.Surface((WIDTH/4, TILE_H*0.5))
+        self.image.fill(BEAM)
         self.rect = self.image.get_rect()
-        self.rect.x = -50
+        self.rect.x = 0
         self.rect.centery = self.player.rect.centery
-        self.chargingTime = 200
-        self.standingTime = 50
+        self.chargingTime = 100
+        self.firstTime = True
 
     def update(self):
-
         if self.chargingTime <=0:
-            self.image = pygame.Surface((WIDTH, TILE_H*1.5))
-            self.image.fill(WHITE)
+            
+            if self.firstTime:
+                self.image = self.beam_frame
+                self.image = pygame.transform.scale(self.image,(int(WIDTH/2),TILE_H*4))
+                self.rect = self.image.get_rect(midright=(-50,self.rect.y))
+
+            self.firstTime = False
+            
+            self.rect.x += 8
+            
+            if self.rect.left > WIDTH:
+                self.kill()
+
             hits = pygame.sprite.collide_rect(self.player, self)
             if hits:
                 self.player.playerDied()
 
-            if self.standingTime <= 0:
-                self.kill()
-            else:
-                self.standingTime -= 1
 
         else:
+            self.rect.centery = self.player.rect.centery
             self.chargingTime -= 1
